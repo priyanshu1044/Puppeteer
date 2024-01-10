a4f97cc08d78be51565ee66b768296881b9cc26b
priyanshupatel1044@gmail.com <priyanshupatel1044@gmail.com>
2024-01-10 13:00:27 +0530

    Delete unnecessary Puppeteer installation command and update dependencies

---
+++ README.md
@@ -0,0 +1,19 @@
+# Puppeteer Learning Project
+
+## Description
+
+This project uses Puppeteer for web scraping. Puppeteer is a Node.js library which provides a high-level API to control Chrome or Chromium over the DevTools Protocol.
+
+## Installation
+
+Before you start, make sure you have Node.js and npm installed on your machine.
+
+1. Clone this repository:
+```bash
+git clone https://github.com/priyanshu1044/Puppeteer.git
+cd <project-directory>
+```
+2. Install the dependencies:
+```bash
+pnpm install --ignore-scripts puppeteer
+```
--- puppeteer-amazon-data/index.js
+++ puppeteer-amazon-data/index.js
@@ -1,26 +1,38 @@
-const puppeteer = require("puppeteer");
-const fs = require("fs");
+const puppeteer = require('puppeteer');
+const fs = require('fs');
 
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    defaultViewport: false,
-    userDataDir: "./user_data",
-  });
-  const page = await browser.newPage();
-  await page.goto(
-    "https://www.amazon.in/s?k=amazon+basic&crid=1Z0DQYR39MP1E&sprefix=amazon+basic+%2Caps%2C383&ref=nb_sb_noss_2"
-  );
-  const productHandles = await page.$$(
-    ".s-main-slot .s-result-list .s-search-results .sg-row"
-  );
-  for (const producthandle of productHandles) {
-    const title = await page.evaluate(
-      (el) =>
-        el.querySelector(".a-size-base-plus .a-color-base .a-text-normal")
-          .textContent,
-      producthandle
-    );
-    console.log(title);
-  }
-})();
+async function scrapeProduct() {
+    const browser = await puppeteer.launch({ headless: false, defaultViewport: null });
+    const page = await browser.newPage();
+
+    // Navigate to a website
+    await page.goto(`https://www.amazon.in/gp/bestsellers/automotive/ref=zg_bs_automotive_sm`, { waitUntil: 'networkidle0' });
+
+    const productHandles = await page.$$(".p13n-gridRow._cDEzb_grid-row_3Cywl");
+
+    // Extracting all links
+    let links = [];
+
+    for (let productHandle of productHandles) {
+        const productLinks = await productHandle.$$eval('.a-link-normal', (elements) =>
+            elements.map(element => ({
+                href: element.href || '', // Use empty string as fallback
+            }))
+        );
+
+        links = links.concat(productLinks);
+    }
+    
+    const loadedLinks = links.filter(link => link.href.trim() !== '');
+
+    const output = {
+        links: loadedLinks,
+    };
+
+    // Convert the object to JSON
+    fs.writeFileSync('output.json', JSON.stringify(output, null, 2));
+
+    await browser.close();
+}
+
+scrapeProduct();
